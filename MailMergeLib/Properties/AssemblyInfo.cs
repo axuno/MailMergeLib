d28bd7ace13b7eb4dc6c8b82b3757253151df03d
Norbert Bietsch <axunonb@users.noreply.github.com>
2016-08-08 20:39:30 +0200

    Add files via upload

--- MailMergeLib/Properties/AssemblyInfo.cs
+++ MailMergeLib/Properties/AssemblyInfo.cs
@@ -1,1 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("MailMergeLib")]
+[assembly: AssemblyDescription(".NET library for email merge")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("axuno gGmbH")]
+[assembly: AssemblyProduct("MailMergeLib 5.0")]
+[assembly: AssemblyCopyright("Copyright © 2007-2016 by Norbert Bietsch")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("493782f8-2007-4316-b72c-28fe39c3180f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("5.0.*")]
+[assembly: AssemblyFileVersion("5.0.0.00")]
