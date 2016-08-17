42ae365e6f8b645670e7a507ecf8d7c51af5c8fe
Norbert Bietsch <axunonb@users.noreply.github.com>
2016-08-17 23:09:54 +0200

    Replaced TextVariableManager with SmartFormatMail

---
+++ MailMergeLib/SmartFormatMail/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Scott.Rippey,SeriousM")]
+[assembly: AssemblyTitle("SmartFormat")]
+[assembly: AssemblyCopyright("Copyright 2015 Scott Rippey")]
+[assembly: AssemblyDescription("A string composition library written in C# that can format data into a string with a minimal, intuitive syntax. It uses extensions to provide named placeholders, pluralization, gender conjugation, and time and list formatting.")]
+[assembly: AssemblyVersion("1.6.1.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
