5e017648eea2e5afd45b1ebd9c6924925022dc6e
Norbert Bietsch <axunonb@users.noreply.github.com>
2016-08-17 23:04:34 +0200

    Replaced TextVariableManager with SmartFormatMail

---
+++ MailMergeLib/SmartFormatMail/Core/Formatting/FormatCache.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MailMergeLib.SmartFormatMail.Core.Parsing;
+
+namespace MailMergeLib.SmartFormatMail.Core.Formatting
+{
+	/// <summary>
+	/// Caches information about a format operation
+	/// so that repeat calls can be optimized to run faster.
+	/// </summary>
+	public class FormatCache
+	{
+		public FormatCache(Format format)
+		{
+			this.Format = format;
+		}
+		/// <summary>
+		/// Caches the parsed format.
+		/// </summary>
+		public Format Format { get; private set; }
+
+		private Dictionary<string, object> cachedObjects;
+		/// <summary>
+		/// Storage for any misc objects.
+		/// This can be used by extensions that want to cache data,
+		/// such as reflection information.
+		/// </summary>
+		public Dictionary<string, object> CachedObjects { get { return cachedObjects ?? (cachedObjects = new Dictionary<string, object>()); } }
+	}
+}
---
+++ MailMergeLib/SmartFormatMail/Core/Formatting/FormattingException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MailMergeLib.SmartFormatMail.Core.Parsing;
+
+namespace MailMergeLib.SmartFormatMail.Core.Formatting
+{
+	/// <summary>
+	/// An exception caused while attempting to output the format.
+	/// </summary>
+	public class FormattingException : Exception
+	{
+		public FormattingException(FormatItem errorItem, Exception formatException, int index)
+		{
+			this.Format = errorItem.baseString;
+			this.ErrorItem = errorItem;
+			this.Issue = formatException.Message;
+			this.Index = index;
+		}
+		public FormattingException(FormatItem errorItem, string issue, int index)
+		{
+			this.Format = errorItem.baseString;
+			this.ErrorItem = errorItem;
+			this.Issue = issue;
+			this.Index = index;
+		}
+
+		public string Format { get; private set; }
+		public FormatItem ErrorItem { get; private set; }
+		public string Issue { get; private set; }
+		public int Index { get; private set; }
+
+		public override string Message
+		{
+			get
+			{
+				return string.Format("Error parsing format string: {0} at {1}\n{2}\n{3}",
+									 Issue,
+									 Index,
+									 Format,
+									 new String('-', Index) + "^");
+			}
+		}
+	}
+}
