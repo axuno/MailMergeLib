78f9269655d9972cf394edd43bae99a473f724cb
Norbert Bietsch <axunonb@users.noreply.github.com>
2016-08-17 23:06:05 +0200

    Replaced TextVariableManager with SmartFormatMail

---
+++ MailMergeLib/SmartFormatMail/Core/Parsing/FormatItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MailMergeLib.SmartFormatMail.Core.Parsing
+{
+	/// <summary>
+	/// Base class that represents a substring
+	/// of text from a parsed format string.
+	/// </summary>
+	public abstract class FormatItem
+	{
+		protected FormatItem(FormatItem parent, int startIndex) : this(parent.baseString, startIndex, parent.baseString.Length)
+		{ }
+
+		protected FormatItem(string baseString, int startIndex, int endIndex)
+		{
+			this.baseString = baseString;
+			this.startIndex = startIndex;
+			this.endIndex = endIndex;
+		}
+
+		[Obsolete("Please use RawText instead")]
+		public string Text { get { return RawText; } }
+
+		/// <summary>
+		/// Retrieves the raw text that this item represents.
+		/// </summary>
+		public string RawText
+		{
+			get
+			{
+				return this.baseString.Substring(startIndex, endIndex - startIndex);
+			}
+		}
+
+
+		public readonly string baseString;
+		public int startIndex;
+		public int endIndex;
+
+		public override string ToString()
+		{
+			if (endIndex <= startIndex)
+				return string.Format("Empty ({0})", baseString.Substring(startIndex));
+			return string.Format("{0}", baseString.Substring(startIndex, endIndex - startIndex));
+		}
+	}
+}
