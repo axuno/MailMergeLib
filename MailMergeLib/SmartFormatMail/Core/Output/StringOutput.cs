d9f63a4e09d1ed8f319383daa0fca62d93621fad
Norbert Bietsch <axunonb@users.noreply.github.com>
2016-08-17 23:05:20 +0200

    Replaced TextVariableManager with SmartFormatMail

---
+++ MailMergeLib/SmartFormatMail/Core/Output/StringOutput.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using MailMergeLib.SmartFormatMail.Core.Extensions;
+
+namespace MailMergeLib.SmartFormatMail.Core.Output
+{
+	/// <summary>
+	/// Wraps a StringBuilder so it can be used for output.
+	/// This is used for the default output.
+	/// </summary>
+	public class StringOutput : IOutput
+	{
+		private readonly StringBuilder output;
+		public StringOutput()
+		{
+			this.output = new StringBuilder();
+		}
+		public StringOutput(int capacity)
+		{
+			this.output = new StringBuilder(capacity);
+		}
+		public StringOutput(StringBuilder output)
+		{
+			this.output = output;
+		}
+
+
+		public void Write(string text, IFormattingInfo formattingInfo)
+		{
+			output.Append(text);
+		}
+		public void Write(string text, int startIndex, int length, IFormattingInfo formattingInfo)
+		{
+			output.Append(text, startIndex, length);
+		}
+
+
+		/// <summary>
+		/// Returns the results of the StringBuilder.
+		/// </summary>
+		public override string ToString()
+		{
+			return output.ToString();
+		}
+	}
+}
--- MailMergeLib/SmartFormatMail/Core/Output/TextWriterOutput.cs
+++ MailMergeLib/SmartFormatMail/Core/Output/TextWriterOutput.cs
@@ -1,1 +1,27 @@
+﻿using System.IO;
+using MailMergeLib.SmartFormatMail.Core.Extensions;
 
+namespace MailMergeLib.SmartFormatMail.Core.Output
+{
+	/// <summary>
+	/// Wraps a TextWriter so that it can be used for output.
+	/// </summary>
+	public class TextWriterOutput : IOutput
+	{
+		public TextWriterOutput(TextWriter output)
+		{
+			Output = output;
+		}
+		public TextWriter Output { get; private set; }
+
+		public void Write(string text, IFormattingInfo formattingInfo)
+		{
+			Output.Write(text);
+		}
+
+		public void Write(string text, int startIndex, int length, IFormattingInfo formattingInfo)
+		{
+			Output.Write(text.Substring(startIndex, length));
+		}
+	}
+}
