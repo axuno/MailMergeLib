4940846da95404cee31769b16e05918c46c54a43
axunonb <nb@axuno.net>
2016-09-02 22:08:25 +0200

    Assigned final version number 5 to the library

--- MailMergeLib/Properties/AssemblyInfo.cs
+++ MailMergeLib/Properties/AssemblyInfo.cs
@@ -19,7 +19,8 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 
 [assembly: InternalsVisibleTo("UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001007d73450c58a30b94409ec6a1ad78af1337bae462b82c80aeda1e0501506db459cd074beb94cb342a3491687e75e0143e6fce3bfa5cc3221b29017e0f4e5b116d0b135405dad460283413f75e63c6db3b1b074a16d780f013433a7b7883e9760079b6d5a41d5d9a3cff80a3b1e42b1d057c94533c978a625c3a0a933cac9ecda9")]
-//[assembly: InternalsVisibleTo("UnitTests")]
+// for above, both assemblies must be signed!
+// for unsigned assemblies use: [assembly: InternalsVisibleTo("UnitTests")]
 
 /*
 Create PublicKey file:
@@ -44,4 +45,4 @@ sn -tp d:Mailmergelib.snk.PublicKey
 //      Revision
 //
 [assembly: AssemblyVersion("5.0.*")]
-[assembly: AssemblyFileVersion("5.0.0.01")]
+[assembly: AssemblyFileVersion("5.0.0.100")]
