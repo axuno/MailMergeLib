84ed7df7ea0df4fd0e83773c1149bb45d28ac711
axunonb <nb@axuno.net>
2017-04-05 21:37:20 +0200

    Updated License

--- MailMergeLib/nuget/build_package.cmd
+++
@@ -1,5 +0,0 @@
-REM Pack .Net Framework Libraries
-nuget.exe pack MailMergeLib.nuspec
-REM Pack for .Net Core
-"C:\Program Files\dotnet\dotnet.exe" pack ~/project.json --no-build --configuration release
-
